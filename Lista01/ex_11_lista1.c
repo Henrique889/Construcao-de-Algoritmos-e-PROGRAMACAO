9ae717f89f3e437a856ddc02747516a56b1d0fec
HENRIQUE PALMA ALBUQUERQUE <henriquepalma412@gmail.com>
2021-08-24 10:52:12 -0300

    Termino da lista1
    EXs 11 e 12 feitos

---
+++ Lista01/ex_11_lista1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int numero1, numero2;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero1);
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &numero2);
+
+    if(numero2 < numero1){
+        for(int x = numero2; x <= numero1; x++){
+            printf("%d ", x);
+        }
+    }
+    if(numero1 < numero2){
+        for(int x = numero1; x <= numero2; x++){
+            printf("%d ", x);
+        }
+    }
+    
+}
---
+++ Lista01/ex_12_lista1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+        int numero1, numero2,soma = 0;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero1);
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &numero2);
+
+    if(numero2 < numero1){
+        for(int x = numero2; x <= numero1; x++){
+            soma+=x;
+            printf("%d ", x);
+        }
+       printf("\nO resultado da soma eh: %d", soma);
+    }
+    if(numero1 < numero2){
+        for(int x = numero1; x <= numero2; x++){
+             soma+=x;
+            printf("%d ", x);
+        }
+       printf("\nO resultado da soma eh: %d", soma);
+    }
+}
