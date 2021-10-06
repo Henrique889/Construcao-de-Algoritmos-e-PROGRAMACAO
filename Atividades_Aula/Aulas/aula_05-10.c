cee51e8ebc68767e9974c24ceaf1ffe276471ff1
HENRIQUE PALMA ALBUQUERQUE <henriquepalma412@gmail.com>
2021-10-05 22:50:18 -0300

    atv_desafio
    arrumei
    Desafio = a PYTHON

--- Atividades_Aula/Aulas/aula_05-10.c
+++ Atividades_Aula/Aulas/aula_05-10.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h> 
 
-void iniciofimString(char text[]; int inicio; int fim);
+void iniciofimString(char text[], int ini, int fim);
 
 int main (){
-    
-    char nome[] = "henrique";
+    char nome[200] = "henrique";
     int ini = 3;
     int fim = 8; 
 
-    iniciofimString(nome,ini,fim);
+    iniciofimString("henrique",3,8);
 
     return 0;
 
 }
 
-void iniciofimString(char text[]; int inicio; int fim){
+void iniciofimString(char text[], int ini, int fim){
     int tam = strlen(text);
-    char novoNome[tam];
+    char novoNome[8];
     
     int k = 0;
-    for(int i = inicio; i < fim; i++){
+    for(int i = ini; i < fim; i++){
         novoNome[k++] = text[i];
         //printf("%c \n", nome[i]);
     }
-    //iniciofim(nome,novoNome,ini,fim);
-    printf("%s", novoNome);
+    for(int i = 0; i < k; i++){
+        printf("%c", novoNome[i]);
+    }
+    
 }
